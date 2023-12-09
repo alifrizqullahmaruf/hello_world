f4de4219bc2dc998d11af7b882afdca9c483bd43
Alif Rizqullah Maruf <alifrizqullahmaruf2003@mail.ugm.ac.id>
2023-12-09 11:11:02 +0700

    Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'My First App women'),
+      home: const MyHomePage(title: 'My First App Man'),
     );
   }
 }
