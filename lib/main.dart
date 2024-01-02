23e9702664f3bf17cac2d43e6c1c2673193ea14c
Alif Rizqullah Maruf <alifrizqullahmaruf2003@mail.ugm.ac.id>
2024-01-02 11:34:13 +0700

    app bar dan background linear

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 
-
 // buat rapih click shift + alt + f
 //refactor = ctrl + shift + R
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Apa KAbar Boss'),
+        appBar: AppBar(
+          title: const Text("My First App"),
+        ),
+        // ini nambahin background color
+        // backgroundColor: Colors.blueGrey,
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 38, 51, 232),
+                Color.fromARGB(255, 85, 122, 223),
+              ],
+            ),
+          ),
+          child: const Center(
+            child: Text('Alif'),
+          ),
         ),
       ),
     ),
